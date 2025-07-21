2459d2532a72040d35ffae52de339b5ba0aefda7
Roland <Ugoroland99@gmail.com>
2025-07-21 19:22:03 +0100

    add changes

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,23 +3,34 @@ import noteRoutes from "./routes/notesRoutes.js";
 import { connectDB } from "./config/db.js"; // Import the function to connect to the database
 import dotenv from "dotenv";
 import cors from "cors";
+import path from "path";
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+const __dirname = path.resolve();
 
 //middleware
-app.use(express.json()); //this middleware will parse JSON bodies: req.body
-app.use(cors({ origin: "http://localhost:5173" }));
+if (process.env.NODE_ENV !== "production") {
+  app.use(cors({ origin: "http://localhost:5173" }));
+}
 
 //our simple custom middleware
 // app.use((req, re, next) => {
 //   console.log(`Req method is ${req.method} & Req URL is ${req.url}`);
 //   next();
 // });
-
+app.use(express.json()); //this middleware will parse JSON bodies: req.body
 app.use("/api/notes", noteRoutes);
+
+app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+if (process.env.NODE_ENV === "production") {
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
+  });
+}
 
 connectDB().then(() => {
   app.listen(PORT, () => {
