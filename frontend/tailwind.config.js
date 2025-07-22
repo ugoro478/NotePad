1eef10dd4782dc2ab633fdcaf5b20e14d38ff5e5
Roland <Ugoroland99@gmail.com>
2025-07-22 07:44:54 +0100

    Fix TailwindCSS setup for Render

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,8 +1,7 @@
 const daisyui = require("daisyui");
 
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ["./src/**/*.{html,js,ts,jsx,tsx}"],
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
   },
