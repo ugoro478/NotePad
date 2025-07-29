25f08aaf753ba6e13818b173cc8ecc2327c49122
Roland <Ugoroland99@gmail.com>
2025-07-29 16:36:54 +0100

    Set up frontend to connect to backend API

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,10 +1,20 @@
 import axios from "axios";
 
-const BASE_URL =
-  import.meta.env.MODE === "development" ? "http://localhost:3000/api" : "/api";
+const BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
 const api = axios.create({
   baseURL: BASE_URL,
 });
 
 export default api;
+
+// import axios from "axios";
+
+// const BASE_URL =
+//   import.meta.env.MODE === "development" ? "http://localhost:3000/api" : "/api";
+
+// const api = axios.create({
+//   baseURL: BASE_URL,
+// });
+
+// export default api;
