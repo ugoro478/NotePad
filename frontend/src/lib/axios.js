1573abff48f33c776fd461893113aa906ead64fd
Roland <Ugoroland99@gmail.com>
2025-07-29 14:55:57 +0100

    all fixed

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,18 +1,10 @@
 import axios from "axios";
 
+const BASE_URL =
+  import.meta.env.MODE === "development" ? "http://localhost:3000/api" : "/api";
+
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL,
+  baseURL: BASE_URL,
 });
 
 export default api;
-
-// import axios from "axios";
-
-// const BASE_URL =
-//   import.meta.env.MODE === "development" ? "http://localhost:3000/api" : "/api";
-
-// const api = axios.create({
-//   baseURL: BASE_URL,
-// });
-
-// export default api;
